99fd6e39d21ab78851d1a0bab1765dc753a6e434
webmaster01 <csi1@grupogema.com.co>
2021-08-18 14:10:41 -0500

    se crea un modulo para modificar fields service

---
+++ odoo_14.0.latest/src/custom/fms_mod/__manifest__.py
@@ -0,0 +1,18 @@
+
+
+{
+    'name': 'Fms modificado',
+    "summary": "Este modulo modifica servicio de campo, agregando campos y ocultando otros",
+    'version': '14.0.1',
+    'author': 'Allser',
+    'sequence': 0,
+    'depends': ['base', 'fieldservice',
+                ],
+    'data': [
+        'views/codigo.xml',
+    ],
+    'application': True,
+}
+
+
+
