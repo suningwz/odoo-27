b3d181ed6e7f19ef0f6ba1348a1684f0c755a59d
webmaster01 <csi1@grupogema.com.co>
2021-08-20 13:06:10 -0500

    Se suber modulos para temas y modulo para traer los elementos de stock

---
+++ odoo_14.0.latest/src/custom/fms_stock_mod/__manifest__.py
@@ -0,0 +1,20 @@
+
+
+{
+    'name': 'Vizualizar el inventario',
+    'summary': 'Diseño para vizualizar los elementos de stock de la persona',
+    'version': '14.0.1',
+    'author': 'Allser',
+    'sequence': 0,
+    'depends': ['base', 'project'
+                ],
+    'data': [
+        'views/unit.xml',
+        'views/unit2.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
+
+
+
---
+++ odoo_14.0.latest/src/custom/legion_mobile_theme/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'Legion Mobile Theme',
+    'version': '14.0.1.0.1',
+    'summary': 'Legion Mobile Theme',
+    'author': 'Bytelegion',
+    'license': 'AGPL-3',
+    'maintainer': 'Bytelegion',
+    'company': 'Bytelegion',
+    'website': 'https://bytelegions.com',
+    'depends': [
+        'web'
+    ],
+    'category':'Branding',
+    'description': """
+           Legion Mobile Theme
+    """,
+   'data': [
+
+    'views/webclient_template_extend.xml',
+
+    ],
+    'price':0,
+    'currency':'USD',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'images': ['static/description/icon.png','static/description/main_screenshot.png']
+}
