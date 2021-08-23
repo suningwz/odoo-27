a702fae2831214d3dd2a2eb146cfd68b5527d0b0
webmaster01 <csi1@grupogema.com.co>
2021-08-23 08:07:49 -0500

    Se diseña las pestañas que conectan con los elementos del tecnico y la bolsa del tecnico

--- odoo_14.0.latest/src/custom/fms_stock_mod/__manifest__.py
+++ odoo_14.0.latest/src/custom/fms_stock_mod/__manifest__.py
@@ -6,11 +6,10 @@
     'version': '14.0.1',
     'author': 'Allser',
     'sequence': 0,
-    'depends': ['base', 'project'
+    'depends': ['base', 'fieldservice'
                 ],
     'data': [
         'views/unit.xml',
-        'views/unit2.xml',
     ],
     'installable': True,
     'application': True,
--- odoo_14.0.latest/src/custom/fms_stock_mod/models/unit.py
+++ odoo_14.0.latest/src/custom/fms_stock_mod/models/unit.py
@@ -2,15 +2,6 @@ from odoo import api, fields, models, _
 from odoo import _, api, exceptions, fields, models
 
 
-class ejemplo_nueva_clase(models.Model):
-    _inherit = 'project.task'
-    tipo_unidad = fields.Selection([('n/a', 'N/A'), ('atm', 'ATM'), ('edf', 'EDF'), ('ofc', 'OFC')], 'Unidad',
-                                   default='n/a')
-
-    @api.onchange('partner_id')
-    def selected_named_pject(self):
-        self.name = self.partner_id.codigo
-        self.tipo_unidad = self.partner_id.tipo_unidad
 
 
 
