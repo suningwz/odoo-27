2588ae0104ff2b8dac8eb2f42e0975d30b4d7275
webmaster04 <csi2@grupogema.com.co>
2021-09-21 14:19:44 -0500

    Hecho por Brandon martin
    Ya funciona el pdf

---
+++ odoo_14.0.latest/src/custom/Archivador_Servicio_de_Campo/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Boton de archivar',
+    'summary': 'Diseño para archivar elementos en servicio de campo',
+    'version': '14.0.1',
+    'author': 'Allser',
+    'sequence': 0,
+    'depends': ['base', 'fieldservice',
+                ],
+    'data': [
+        'views/unit.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
+
+
+
---
+++ odoo_14.0.latest/src/custom/Archivador_Servicio_de_Campo/models/unit.py
@@ -0,0 +1,19 @@
+import requests
+from odoo import api, fields, models, _
+from odoo import _, api, exceptions, fields, models, http
+from odoo.http import request
+from datetime import datetime
+import json
+from xlsxwriter import app
+from geopy.geocoders import Nominatim
+import time
+import math
+import urllib.request
+
+
+class boton_archivador(models.Model):
+    _inherit = "fsm.order"
+
+    active = fields.Boolean('Active', default=True)
+    def funcion_libre(self):
+        self.active = False
