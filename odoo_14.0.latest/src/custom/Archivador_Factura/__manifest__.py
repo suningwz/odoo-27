cb3f7cad2e20da7dc112129f5157c840b7c1f473
webmaster03 <csi1@grupogema.com.co>
2021-09-21 12:11:48 -0500

    Se arreglo el problema que tenia brandon para ingreasar el bton de archivo

---
+++ odoo_14.0.latest/src/custom/Archivador_Factura/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Boton de archivo',
+    'summary': 'Diseño para archivar elementos',
+    'version': '14.0.1',
+    'author': 'Allser',
+    'sequence': 0,
+    'depends': ['base', 'account',
+                ],
+    'data': [
+        'views/unit.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
+
+
+
