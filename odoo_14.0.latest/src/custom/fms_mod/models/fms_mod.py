5a306b31a61e24d6f0748ce07b20e751f6848664
webmaster03 <csi1@grupogema.com.co>
2021-09-24 07:47:04 -0500

    Se diseño un funcion que alerta al tecnico que se le ah asignado un servicio

--- odoo_14.0.latest/src/custom/fms_mod/models/fms_mod.py
+++ odoo_14.0.latest/src/custom/fms_mod/models/fms_mod.py
@@ -27,6 +27,26 @@ class ejemplo_nueva_clase(models.Model):
         self.tdv = self.project_task_id.partner_id.tdv
         self.description = self.project_task_id.modificacion_descripcion
 
+    @api.onchange('person_id')
+    def messages_person(self):
+        personal =self.env["hr.employee"].search(
+            [("name", "=", self.person_id.name)], limit=1
+        )
+        id = str(self.id).split('_')
+        personal.user_id.notify_success(f'Asignacion del proyecto {self.name}')
+        id_personal = self.env["res.partner"].search(
+            [("name", "=", self.person_id.name)], limit=1
+        ).id
+        self.env['mail.message'].create({'message_type': "comment",
+                                         'subtype_id': 1,
+                                         'parent_id': 4172,
+                                         'body': f'<span><a href="http://allser.com.co/web#model=res.partner&amp;id={id_personal}" class="o_mail_redirect" data-oe-id="26438" data-oe-model="res.partner" target="_blank">El tecnico lider asignado es: {personal.name}</a></span>',
+                                         'record_name': f'{self.name}',
+                                         # partner to whom you send notification
+                                         'model': self._name,
+                                         'res_id': int(id[1]),
+                                         })
+
 class descripcion(models.Model):
     _inherit = 'project.task'
 
