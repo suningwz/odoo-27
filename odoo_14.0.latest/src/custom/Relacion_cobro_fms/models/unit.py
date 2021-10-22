a1adb2d32f348b5ff41e3f01c2a7a3726d24a3bd
webmaster03 <csi1@grupogema.com.co>
2021-10-22 09:12:22 -0500

    Se corrigieron errores

--- odoo_14.0.latest/src/custom/Relacion_cobro_fms/models/unit.py
+++ odoo_14.0.latest/src/custom/Relacion_cobro_fms/models/unit.py
@@ -8,19 +8,33 @@ class campo_relacion_cobreo(models.Model):
 
     ventas_fsm = fields.One2many('sale.order', 'fsm_id', string= 'Tabla para crear la orden de corbo')
     def crear_cotizacion(self):
-        """
-        This function returns an action that displays a full FSM Order
-        form when creating an FSM Order from a project.
-        """
-        action = self.env.ref("sale.action_orders")
-        result = action.read()[0]
-        # override the context to get rid of the default filtering
-        result["context"] = {
-            "partner_id": self.centro_costos.owner_id.id,
-        }
-        res = self.env.ref("sale.view_order_form", False)
-        result["views"] = [(res and res.id or False, "form")]
-        return result
+        if not self.centro_costos:
+            view = self.env.ref('sh_message.sh_message_wizard')
+            view_id = view and view.id or False
+            context = dict(self._context or {})
+            context['message'] ="Verificar el centro de costos"
+            return {
+                'name': 'ADVERTENCIA',
+                'type': 'ir.actions.act_window',
+                'view_type': 'form',
+                'view_model': 'form',
+                'res_model': 'sh.message.wizard',
+                'views': [(view.id, 'form')],
+                'view_id': view.id,
+                'target': 'new',
+                'context': context,
+            }
+        else:
+            action = self.env.ref("sale.action_orders")
+            result = action.read()[0]
+            # override the context to get rid of the default filtering
+            result["context"] = {
+                "default_partner_id": self.centro_costos.owner_id.id,
+                "default_fms_id": self.id,
+            }
+            res = self.env.ref("sale.view_order_form", False)
+            result["views"] = [(res and res.id or False, "form")]
+            return result
 
 class _sale_order_line(models.Model):
     _inherit = 'sale.order'
