6a3b99bd9180e86b55c9757663dd99d6baae60b7
marnellsecurit <csi1@grupogema.com.co>
2021-11-22 11:32:25 -0500

    Se modifico la logica para la bolsa de dineros

--- odoo_14.0.latest/src/custom/fms_money_mod/models/unit2.py
+++ odoo_14.0.latest/src/custom/fms_money_mod/models/unit2.py
@@ -8,13 +8,15 @@ class video_mano(models.Model):
 
     bolsa_dineros = fields.Integer('dinero a solicitar')
     bolsa_total = fields.Integer('total')
-    personal = fields.Many2one('res.partner', required = 1)
+    personal = fields.Many2one('res.partner', required=1)
     estado = fields.Selection([
         ('p1', "Solicitado"),
         ('p2', "Aprobado"),
         ('p3', "Cancelado"),
+        ('p4', "Guardado")
     ], default='p1')
     motivo = fields.Text()
+
     def _default_employee(self):
         return self.env.user.employee_id
 
@@ -38,29 +40,43 @@ class video_mano(models.Model):
         self.bolsa_total = persona.bolsa_total
 
     def aprovacion_exporte(self):
-        persona = self.env["hr.employee"].search(
-            [("name", "=", self.personal.name)], limit=1
-        )
-        valor = self.bolsa_dineros
-        valor2 = self.bolsa_total
-        valor = valor2 + valor
-        if valor == 150000:
-            self.bolsa_dineros = 0
-            self.bolsa_total = valor
-            persona.bolsa_total = self.bolsa_total
-        elif valor < 150000:
-            self.bolsa_dineros = 0
-            raise ValidationError(f'El valor de: {valor} es menor al maximo')
-        elif valor > 150000:
-            if valor <= 300000:
-                self.bolsa_dineros = 0
+        if self.estado == 'p2':
+            view = self.env.ref('sh_message.sh_message_wizard')
+            view_id = view and view.id or False
+            context = dict(self._context or {})
+            context[
+                'message'] = "ya se encuentra aprobado"
+            return {
+                'name': 'ADVERTENCIA',
+                'type': 'ir.actions.act_window',
+                'view_type': 'form',
+                'view_model': 'form',
+                'res_model': 'sh.message.wizard',
+                'views': [(view.id, 'form')],
+                'view_id': view.id,
+                'target': 'new',
+                'context': context,
+            }
+        else:
+            persona = self.env["hr.employee"].search(
+                [("name", "=", self.personal.name)], limit=1
+            )
+            valor = self.bolsa_dineros
+            valor2 = self.bolsa_total
+            valor = valor2 + valor
+            if valor == 150000:
                 self.bolsa_total = valor
                 persona.bolsa_total = self.bolsa_total
-            elif valor > 300000:
-                self.bolsa_dineros = 0
-                raise ValidationError(f'El valor de: {valor} excede el tope')
+            elif valor < 150000:
+                raise ValidationError(f'El valor de: {valor} es menor al maximo')
+            elif valor > 150000:
+                if valor <= 300000:
+                    self.bolsa_total = valor
+                    persona.bolsa_total = self.bolsa_total
+                elif valor > 300000:
+                    raise ValidationError(f'El valor de: {valor} excede el tope')
 
-        self.estado = 'p2'
+            self.estado = 'p2'
 
     def cancelado(self):
         self.estado = 'p3'
@@ -71,3 +87,7 @@ class video_mano(models.Model):
             [("name", "=", name_location)], limit=1
         )
         personal.user_id.notify_success(f'Nueva solicitud de dineros')
+
+    @api.constrains('bolsa_dineros')
+    def cambio_guardado(self):
+        self.estado = 'p4'
