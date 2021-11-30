281d2ccb21cf26ad454f2305f02667e7693b9bdb
marnellsecurit <csi1@grupogema.com.co>
2021-11-30 11:32:46 -0500

    Cambios esteticos del codigo

--- odoo_14.0.latest/src/custom/mantenimientos/models/models.py
+++ odoo_14.0.latest/src/custom/mantenimientos/models/models.py
@@ -20,30 +20,23 @@ class mantenimientos(models.Model):
     codificador = fields.Selection([('p1', 'p1'), ('p2', 'p1')], 'codificador', default='p1')
     ordenes_fsm = fields.One2many('fsm.order', 'mantenimientos')
     fecha_h_liberacion = fields.Datetime('F/H de liberacion')
-
     @api.onchange('fecha_h_liberacion')
     def hora_liberacion_campos(self):
         self.request_date = self.fecha_h_liberacion
-
     def formato_identificador(self):
         if not self.id:
             dato = "Nuevo"
         else:
             dato = self.id
         return dato
-
     num_identificado = fields.Char('Identificacion interna', default=formato_identificador)
-
     @api.onchange('name')
     def asignar_name(self):
         if not self.name:
             self.name = self.num_identificado
-
     @api.onchange('fecha_inicio')
     def asignarfechas(self):
         self.schedule_date = self.fecha_inicio
-
-    @api.constrains('name')
     def buscar_cajeros(self):
         cajero = self.env["res.partner"].search(
             [("parent_id", "=", self.cliente.name), ("codigo", "=", self.codigo),
@@ -71,77 +64,12 @@ class mantenimientos(models.Model):
             self.ciudad_cajero = cajero.city
 
         self.codificador = 'p2'
-        if not self.request_date:
-            print("No se puede")
-        else:
-            tomorrow = datetime.now()
-            dia = self.request_date
-            cont = 1
-            for x in range(5):
-                cont = cont + 1
-                tomorrow = dia + timedelta(cont)
-                wek = day_name[tomorrow.weekday()]
-                d1 = tomorrow.strftime("%d/%m")
-                if wek == "Saturday":
-                    cont = cont + 1
-                elif wek == "Sunday":
-                    cont = cont + 1
-                elif d1 == "01/01":
-                    cont = cont + 1
-                elif d1 == "11/01":
-                    cont = cont + 1
-                elif d1 == "22/03":
-                    cont = cont + 1
-                elif d1 == "01/04":
-                    cont = cont + 1
-                elif d1 == "02/04":
-                    cont = cont + 1
-                elif d1 == "01/05":
-                    cont = cont + 1
-                elif d1 == "17/05":
-                    cont = cont + 1
-                elif d1 == "07/06":
-                    cont = cont + 1
-                elif d1 == "14/06":
-                    cont = cont + 1
-                elif d1 == "05/07":
-                    cont = cont + 1
-                elif d1 == "20/07":
-                    cont = cont + 1
-                elif d1 == "07/08":
-                    cont = cont + 1
-                elif d1 == "16/08":
-                    cont = cont + 1
-                elif d1 == "18/10":
-                    cont = cont + 1
-                elif d1 == "01/11":
-                    cont = cont + 1
-                elif d1 == "15/11":
-                    cont = cont + 1
-                elif d1 == "08/12":
-                    cont = cont + 1
-                elif d1 == "25/12":
-                    cont = cont + 1
-            print(self.maintenance_team_id.lider.id)
-            data = {
-                'res_id': self.id,
-                'user_id': self.maintenance_team_id.lider.id,
-                'res_model_id': 444,
-                'summary': 'Nueva solicitud',
-                'activity_type_id': 4,
-                'date_deadline': tomorrow
-            }
-            if self.name == 'Nuevo':
-                creacion_tarea = self.env['mail.activity'].create(data)
-
     def crear_fsm(self):
-
         if self.stage_id != 1:
             self.stage_id = 2
         action = self.env.ref("fieldservice.action_fsm_operation_order")
         result = action.read()[0]
         # parametros que se le pasan por contexto
-
         result["context"] = {
             "default_project_id": self.id,
             "default_mantenimientos": self.id,
@@ -150,7 +78,6 @@ class mantenimientos(models.Model):
         res = self.env.ref("fieldservice.fsm_order_form", False)
         result["views"] = [(res and res.id or False, "form")]
         return result
-
     @api.constrains('num_identificado')
     def asignacion_formato(self):
         if self.num_identificado == 'Nuevo':
@@ -171,7 +98,67 @@ class mantenimientos(models.Model):
                             if c.id != self.id:
                                 raise Warning('El numero de solicitud ya existe, favor validar')
             self.user_id = self.maintenance_team_id.lider
-
+            if not self.request_date:
+                print("No se puede")
+            else:
+                tomorrow = datetime.now()
+                dia = self.request_date
+                cont = 1
+                for x in range(5):
+                    cont = cont + 1
+                    tomorrow = dia + timedelta(cont)
+                    wek = day_name[tomorrow.weekday()]
+                    d1 = tomorrow.strftime("%d/%m")
+                    if wek == "Saturday":
+                        cont = cont + 1
+                    elif wek == "Sunday":
+                        cont = cont + 1
+                    elif d1 == "01/01":
+                        cont = cont + 1
+                    elif d1 == "11/01":
+                        cont = cont + 1
+                    elif d1 == "22/03":
+                        cont = cont + 1
+                    elif d1 == "01/04":
+                        cont = cont + 1
+                    elif d1 == "02/04":
+                        cont = cont + 1
+                    elif d1 == "01/05":
+                        cont = cont + 1
+                    elif d1 == "17/05":
+                        cont = cont + 1
+                    elif d1 == "07/06":
+                        cont = cont + 1
+                    elif d1 == "14/06":
+                        cont = cont + 1
+                    elif d1 == "05/07":
+                        cont = cont + 1
+                    elif d1 == "20/07":
+                        cont = cont + 1
+                    elif d1 == "07/08":
+                        cont = cont + 1
+                    elif d1 == "16/08":
+                        cont = cont + 1
+                    elif d1 == "18/10":
+                        cont = cont + 1
+                    elif d1 == "01/11":
+                        cont = cont + 1
+                    elif d1 == "15/11":
+                        cont = cont + 1
+                    elif d1 == "08/12":
+                        cont = cont + 1
+                    elif d1 == "25/12":
+                        cont = cont + 1
+                print(self.maintenance_team_id.lider.id)
+                data = {
+                    'res_id': self.id,
+                    'user_id': self.maintenance_team_id.lider.id,
+                    'res_model_id': 444,
+                    'summary': 'Nueva solicitud',
+                    'activity_type_id': 4,
+                    'date_deadline': tomorrow
+                }
+                creacion_tarea = self.env['mail.activity'].create(data)
 
 class conection_fsm(models.Model):
     _inherit = 'fsm.order'
