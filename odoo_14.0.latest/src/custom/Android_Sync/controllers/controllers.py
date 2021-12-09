a6f40aaa6ed154cb521621fab684a192f66ecb69
Webmaster04 <csi2@grupogema.com.co>
2021-12-09 15:50:25 -0500

    ajustes

--- odoo_14.0.latest/src/custom/Android_Sync/controllers/controllers.py
+++ odoo_14.0.latest/src/custom/Android_Sync/controllers/controllers.py
@@ -1,9 +1,49 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
+import json
+from odoo.http import Response
 
 
 class AndroidSync(http.Controller):
+    #Ruta personalizada  (' /contacto' ) 
+    #Auth: Permisos de quien puede entrar a esta ruta
     @http.route('/contacto', auth='public', website= True)
+    #Nombre de la funcion ojala no involcarla otravez porque se sobre-escribiria
     def contacto_redirect(self):
-        return request.redirect('/contactus')
+        #Funcion redirect :  La url del render real
+        return request.redirect('/contactus') 
+    
+class  WebsiteDirecto(http.Controller):
+    @http.route('/productos/<model("product.template"):product>', auth='public', website=True)
+    def product(self, product):
+        return http.request.render('Android_Sync.product',{
+            "product":product
+        })
+        
+class OdooAndroidSync(http.Controller):
+
+    @http.route('/api/attendance/hours/<user_id>/<day_week>', auth='user', methods=['GET'])
+    def get_attendance_hours(self, user_id, day_week, **kw):
+        try:
+            calendar_id = http.request.env['hr.employee'].sudo().search([('user_id', '=', int(user_id))]).resource_calendar_id.id
+            hours = http.request.env['resource.calendar.attendance'].sudo().search_read([('calendar_id', '=', int(calendar_id)),  
+                ('dayofweek', '=', day_week)],['hour_from', 'hour_to'])
+
+            return self.build_response(hours)
+        except Exception as e:
+            return self.build_response({'err': str(e)})
+
+    @http.route('/api/attendance', auth='user', methods=['POST'], csrf=False)
+    def insert_attendance(self, **kw):
+        try:
+            attendance = json.loads(str(http.request.httprequest.data, 'utf-8'))
+            http.request.env['hr.attendance'].create(attendance)
+            return self.build_response({'message': 'inserted'})
+
+        except Exception as e:
+            return self.build_response({'err': str(e)})
+
+    def build_response(self, entity):
+        response = json.dumps(entity, ensure_ascii=False).encode('utf8')
+        return Response(response, content_type='application/json;charset=utf-8', status=200)   
