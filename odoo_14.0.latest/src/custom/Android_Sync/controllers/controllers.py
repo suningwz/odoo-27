5bd0aad3dc30c96ac9421d764a95309de1ac31c4
WebMaster04 <csi2@grupogema.com.co>
2021-12-13 08:40:22 -0500

    pruebas controladores android

--- odoo_14.0.latest/src/custom/Android_Sync/controllers/controllers.py
+++ odoo_14.0.latest/src/custom/Android_Sync/controllers/controllers.py
@@ -6,29 +6,35 @@ from odoo.http import Response
 
 
 class AndroidSync(http.Controller):
-    #Ruta personalizada  (' /contacto' ) 
-    #Auth: Permisos de quien puede entrar a esta ruta
-    @http.route('/contacto', auth='public', website= True)
-    #Nombre de la funcion ojala no involcarla otravez porque se sobre-escribiria
+    # Ruta personalizada  (' /contacto' )
+    # Auth: Permisos de quien puede entrar a esta ruta
+    @http.route('/contactus', auth='public', website=True)
+    # Nombre de la funcion ojala no involcarla otravez porque se sobre-escribiria
     def contacto_redirect(self):
-        #Funcion redirect :  La url del render real
-        return request.redirect('/contactus') 
-    
-class  WebsiteDirecto(http.Controller):
+        # Funcion redirect :  La url del render real
+        return request.redirect('/contacto')
+
+    @http.route('/contacto', auth='public', website=True)
+    def contacto(self):
+        return "Hola mundo "
+
+
+class WebsiteDirecto(http.Controller):
     @http.route('/productos/<model("product.template"):product>', auth='public', website=True)
     def product(self, product):
-        return http.request.render('Android_Sync.product',{
-            "product":product
+        return http.request.render('Android_Sync.product', {
+            "product": product
         })
-        
-class OdooAndroidSync(http.Controller):
 
+
+class OdooAndroidSync(http.Controller):
     @http.route('/api/attendance/hours/<user_id>/<day_week>', auth='user', methods=['GET'])
     def get_attendance_hours(self, user_id, day_week, **kw):
         try:
-            calendar_id = http.request.env['hr.employee'].sudo().search([('user_id', '=', int(user_id))]).resource_calendar_id.id
-            hours = http.request.env['resource.calendar.attendance'].sudo().search_read([('calendar_id', '=', int(calendar_id)),  
-                ('dayofweek', '=', day_week)],['hour_from', 'hour_to'])
+            calendar_id = http.request.env['hr.employee'].sudo().search(
+                [('user_id', '=', int(user_id))]).resource_calendar_id.id
+            hours = http.request.env['resource.calendar.attendance'].sudo().search_read([('calendar_id', '=', int(calendar_id)),
+                                                                                         ('dayofweek', '=', day_week)], ['hour_from', 'hour_to'])
 
             return self.build_response(hours)
         except Exception as e:
@@ -37,7 +43,8 @@ class OdooAndroidSync(http.Controller):
     @http.route('/api/attendance', auth='user', methods=['POST'], csrf=False)
     def insert_attendance(self, **kw):
         try:
-            attendance = json.loads(str(http.request.httprequest.data, 'utf-8'))
+            attendance = json.loads(
+                str(http.request.httprequest.data, 'utf-8'))
             http.request.env['hr.attendance'].create(attendance)
             return self.build_response({'message': 'inserted'})
 
@@ -46,4 +53,25 @@ class OdooAndroidSync(http.Controller):
 
     def build_response(self, entity):
         response = json.dumps(entity, ensure_ascii=False).encode('utf8')
-        return Response(response, content_type='application/json;charset=utf-8', status=200)   
+        return Response(response, content_type='application/json;charset=utf-8', status=200)
+
+
+@http.route('/get_reports', type="json", auth='public', csrf=False)
+def get_all_reports(self):
+     # get all  reports
+     reports_rec = request.env['fsm.order'].search([])
+     reports= []
+     for rec in reports_rec:
+            vals = {
+                'id': rec.id,
+                'name': rec.name,
+                'subject': rec.subject_id.name,
+                'phone': rec.phone_number,
+            }
+            reports.append(vals)
+     data = {
+            'status': "200",
+            'message': "success",
+            'response': reports,
+        }
+     return data
--- odoo_14.0.latest/src/custom/Android_Sync/models/models.py
+++ odoo_14.0.latest/src/custom/Android_Sync/models/models.py
@@ -1,18 +1,12 @@
 # -*- coding: utf-8 -*-
+from odoo import models, fields, api
 
-# from odoo import models, fields, api
+class Reports(models.Model):
+     _name = 'reports'
+     _inherit = ['fsm.order']
+     _description = "reports model"
+     
+     name = fields.Char(string="Nombre del Reporte", required=True)
+     template_id = fields.Char(string="Plantilla", required=True)
+     person_id = fields.Char(string="Asignado a : ", required=True)
 
-
-# class android__sync(models.Model):
-#     _name = 'android__sync.android__sync'
-#     _description = 'android__sync.android__sync'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
